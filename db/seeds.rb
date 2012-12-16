16491b764220ca70226136949fddd6eb551fb2fc
KTakata <xxamagixx@gmail.com>
2012-12-17 05:27:39 +0900

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# -*- encoding : utf-8 -*-
+#初期ユーザー作成
+user = User.create!(name: "admin", nick_name: "Admin User", email: "root@admin.com", password: "root")
+
+#TODO:開発がすすんだら別のrakeコマンドに移す
+#初期ユーザー所属テストシナリオ作成
+progress_session = TSession.create!(system_name: "ダブルクロス The 3rd Edition", scenario_name: "テストシナリオ", start_flag: false, end_flag: false)
+past_session = TSession.create!(system_name: "アリアンロッドRPG 2E", scenario_name: "テストシナリオ2", start_flag: true, end_flag: true)
+Player.create!(user_id: user.id, t_session_id: progress_session.id, player_type: "Game Master")
+Player.create!(user_id: user.id, t_session_id: past_session.id, player_type: "Player 1st")
