46fb5ae1c4222a6ee4e83005e5c6ecefaa59e8d5
KTakata <xxamagixx@gmail.com>
2012-12-17 22:15:01 +0900

    set find_current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_filter :find_current_user
   def user
     if session[:user_id]
       user = User.find(session[:user_id])
@@ -9,5 +10,10 @@ class ApplicationController < ActionController::Base
     if user.nil?
       redirect_to login_url
     end
+  end
+
+  private
+  def find_current_user
+    @current_user = user if user
   end
 end
