ef3505e26c55d033e5c9d07f2d76f3a63e119334
KTakata <xxamagixx@gmail.com>
2012-12-17 22:43:42 +0900

    Modified seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,11 @@ new_session = TSession.create!(system_name: "ダブルクロス The 3rd Edition"
 recruitment_session = TSession.create!(system_name: "異界戦記カオスフレアSC", scenario_name: "テストシナリオ2", t_session_status: "recruit")
 progress_session = TSession.create!(system_name: "ソード・ワールド2.0", scenario_name: "テストシナリオ3", t_session_status: "progress")
 past_session = TSession.create!(system_name: "アリアンロッドRPG 2E", scenario_name: "テストシナリオ4", t_session_status: "end")
-Player.create!(user_id: user.id, t_session_id: new_session.id, player_type: "Game Master")
-Player.create!(user_id: user.id, t_session_id: recruitment_session.id, player_type: "Game Master")
-Player.create!(user_id: user.id, t_session_id: progress_session.id, player_type: "Game Master")
-Player.create!(user_id: user.id, t_session_id: past_session.id, player_type: "Game Master")
+Player.create!(user_id: user.id, t_session_id: new_session.id, player_type: "Game Master", player_status: "join")
+Player.create!(user_id: user.id, t_session_id: recruitment_session.id, player_type: "Game Master", player_status: "join")
+Player.create!(user_id: user.id, t_session_id: progress_session.id, player_type: "Game Master", player_status: "join")
+Player.create!(user_id: user.id, t_session_id: past_session.id, player_type: "Game Master", player_status: "join")
+
+user = User.create!(name: "test_user", nick_name: "Test Player", email: "test@player.com", password: "test")
+Player.create!(user_id: user.id, t_session_id: recruitment_session.id, player_type: "Player 1st", player_status: "rsvp")
+
