6dfeb244714f92ebdbae868d0f3d30d29d504f75
KTakata <xxamagixx@gmail.com>
2012-12-22 16:41:13 +0900

    Change " to '

--- app/controllers/t_sessions_controller.rb
+++ app/controllers/t_sessions_controller.rb
@@ -10,9 +10,10 @@ before_filter :find_current_player, only: ['join_request', 'cancel_request']
 
   def show
     @t_session = TSession.find(params[:id])
-    @rsvp_players = Player.where(t_session_id: @t_session.id, player_status: "rsvp")
-    @join_players = Player.where(t_session_id: @t_session.id, player_status: "join")
+    @rsvp_players = Player.where(t_session_id: @t_session.id, player_status: 'rsvp')
+    @join_players = Player.where(t_session_id: @t_session.id, player_status: 'join')
     @current_player = Player.find_by_user_id_and_t_session_id(@current_user.id, @t_session.id)
+    #参加していプレイヤー表示用
     @join_players_no = []
     @join_players.each do |player|
       @join_players_no << player.player_status[7..7] if player.player_status
@@ -45,16 +46,16 @@ before_filter :find_current_player, only: ['join_request', 'cancel_request']
 
   #ここではセッションに参加希望するユーザーを設定する
   def join_request
-    Player.create!(user_id: @current_user.id, t_session_id: @t_session.id, player_status: "rsvp") unless @player
+    Player.create!(user_id: @current_user.id, t_session_id: @t_session.id, player_status: 'rsvp') unless @player
     if @player.player_status == nil
-      @player.player_status = "rsvp"
+      @player.player_status = 'rsvp'
       @player.save!
     end
     redirect_to  t_session_path(@t_session.id)
   end
 
   def cancel_request
-    raise "Unkown current player" unless @player
+    raise 'Unkown current player' unless @player
     @player.player_status = nil
     @player.save!
     redirect_to  t_session_path(@t_session.id)
