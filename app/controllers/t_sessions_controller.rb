628e227a8983b01ac911b94f5f18b44883a8e061
KTakata <xxamagixx@gmail.com>
2012-12-22 11:52:41 +0900

    adjustment

--- app/controllers/t_sessions_controller.rb
+++ app/controllers/t_sessions_controller.rb
@@ -1,15 +1,21 @@
+# -*- encoding : utf-8 -*-
 class TSessionsController < ApplicationController
   def index
   end
 
   def new
-  	@t_session = TSession.new
+    @t_session = TSession.new
   end
 
   def show
     @t_session = TSession.find(params[:id])
-    @players = Player.where(t_session_id: @t_session.id)
+    @rsvp_players = Player.where(t_session_id: @t_session.id, player_status: "rsvp")
+    @join_players = Player.where(t_session_id: @t_session.id, player_status: "join")
     @current_player = Player.find_by_user_id_and_t_session_id(@current_user.id, @t_session.id)
+    @join_players_no = []
+    @join_players.each do |player|
+      @join_players_no < player.player_status[7..7] if player.player_status
+    end
   end
 
   def create
