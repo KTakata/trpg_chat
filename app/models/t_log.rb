f6e77bc39796837761fc5aee77dc0bd0e6c0e933
KTakata <xxamagixx@gmail.com>
2012-12-26 00:57:22 +0900

    Fix bug Association name!

--- app/models/t_log.rb
+++ app/models/t_log.rb
@@ -1,6 +1,6 @@
 class TLog < ActiveRecord::Base
   attr_accessible :body, :owner_id, :t_session_id, :log_type, :many, :d_type, :score, :move_side, :move_vertical
-  belongs_to :user, foreign_key: 'owner_id'
+  belongs_to :player, foreign_key: 'owner_id'
   validates :body, presence: true
  
 end
