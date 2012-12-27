c2e15ae89deaae0e5d6918013a1c59b880b07904
KTakata <xxamagixx@gmail.com>
2012-12-27 20:55:55 +0900

    Check validate to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :nick_name, :password
   has_many :players
   has_many :t_sessions, through: :players
+
+  validates :name, presence: true
+  validates :nick_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 4, maximum: 20}
+
 end
