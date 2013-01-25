602ae0435fd0c3f259d48fd220e9b8807598d715
KTakata <xxamagixx@gmail.com>
2013-01-26 00:31:10 +0900

    change migration file 本当はこんな事してはいけない

--- db/migrate/20121214151912_change_player_type_to_players.rb
+++ db/migrate/20121214151912_change_player_type_to_players.rb
@@ -1,9 +1,9 @@
 class ChangePlayerTypeToPlayers < ActiveRecord::Migration
   def up
-    change_column(:players, :player_type, :string)
+    change_column :players, :player_type, :string
   end
 
   def down
-    change_column(:players, :player_type, :integer)
+    change_column :players, :player_type, :integer
   end
 end
